20ed449d896a08d5237f278b208fb04a20c2a4a6
csxark <arktandoncs@gmail.com>
2025-04-27 03:32:57 +0530

    comments

--- src/components/home/SustainablePractices.jsx
+++ src/components/home/SustainablePractices.jsx
@@ -87,12 +87,12 @@ function SustainablePractices() {
           viewport={{ once: true }}
           className="mt-12 text-center"
         >
-          <a href="/sustainability" className="inline-flex items-center text-primary-500 font-medium hover:text-primary-600 transition-colors">
+          {/* <a href="/sustainability" className="inline-flex items-center text-primary-500 font-medium hover:text-primary-600 transition-colors">
             Learn more about our sustainable practices
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
             </svg>
-          </a>
+          </a> */}
         </motion.div>
       </div>
     </section>
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
-import Announcement from './Announcement'
+// import Announcement from './Announcement'
 import { motion } from 'framer-motion'
 
 function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Announcement message="Free shipping on orders above ₹1999 | Use code INDIE100 for 10% off your first order" />
+      {/* <Announcement message="Free shipping on orders above ₹1999 | Use code INDIE100 for 10% off your first order" /> */}
       <Header />
       <main className="flex-grow">
         <motion.div
